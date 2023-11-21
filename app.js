f42b794d21abdc6e41e9d44d1ed252a70523adb8
hamid1103 <35217917+hamid1103@users.noreply.github.com>
2023-11-21 14:37:03 +0100

    asd

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 import express from 'express'
 import 'dotenv/config'
+import mongoose from 'mongoose'
 
+const mongoDB = "mongodb://localhost:27017/farmGame"
 let app = express();
 let port = process.env.PORT
 
-app.listen(port, () => {
+const connectDB = async ()=>{
+    console.log('Connecting to Mongodb')
+    await mongoose.connect(mongoDB)
+}
+
+connectDB().then(
+    ()=>{
+        console.log("MongoDB Connected")
+        app.listen(port, ()=>{
+            onListen();
+        })
+    }
+);
+
+function onListen(){
     console.log(`Example app listening on port ${port}`)
-})
+}
 
 app.get('/', (req, res) =>{
     res.send("<h2>Hello world</h2>")
 })
+
+
