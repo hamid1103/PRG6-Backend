69fdac10010b9afe73a4256cd7562023f3fae23f
hamid1103 <35217917+hamid1103@users.noreply.github.com>
2023-12-12 14:46:50 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ import {userRouter} from "./Routes/user.js";
 import {farmRouter} from "./Routes/farm.js";
 import {creatorRouter} from "./Routes/creator.js";
 import {cropRouter} from "./Routes/crop.js";
+import * as fs from 'fs';
+import * as https from 'https';
 
 let port = process.env.PORT
 let app = express();
@@ -19,10 +21,25 @@ app.use('/farm', farmRouter);
 app.use('/creator', creatorRouter);
 app.use('/crops', cropRouter)
 
-
-app.listen(port, () => {
-    onListen();
-})
+if(process.env.privatekey){
+    const privateKey = fs.readFileSync(process.env.privatekey,'utf8')
+    const certificate = fs.readFileSync(process.env.certificate, "utf8")
+    const chain = fs.readFileSync(process.env.chain, "utf8")
+    console.log("got files")
+    const credentials = {
+        key: privateKey,
+        cert: certificate,
+        ca: chain
+    };
+    https.createServer(credentials, app).listen(443, ()=>{
+        console.log()
+        onListen()
+    })
+}else {
+    app.listen(port, () => {
+        onListen();
+    })
+}
 
 function onListen() {
     console.log(`Example app listening on port ${port}`)
