594ea1f84e9f52cb8ae9f2689cf4ec37782e3cd9
hamid1103 <35217917+hamid1103@users.noreply.github.com>
2023-12-12 16:22:50 +0100

    Update crop.js

--- Routes/crop.js
+++ Routes/crop.js
@@ -1,14 +1,28 @@
 import express from 'express'
-import {authenticate} from "../Middlewares/auth.js";
+import {adminAuthorize, authenticate} from "../Middlewares/auth.js";
 import {findCrop, findCrops, getCrops} from "../Controllers/cropController.js";
+import {deleteCrop, registerNewCrop} from "../Controllers/Creator/cropControllerCreator.js";
+import {creatorRouter} from "./creator.js";
 
 const cropRouter = express.Router();
+
+creatorRouter.delete('/:name', authenticate, adminAuthorize, deleteCrop)
+
+creatorRouter.options("/:name", (req, res) => {
+    res.header('Allow', 'DELETE');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.sendStatus(200);
+});
+
+creatorRouter.post('/', authenticate, adminAuthorize, registerNewCrop)
 
 cropRouter.get('/', getCrops)
 cropRouter.options("/", (req, res) => {
     res.header('Allow', 'GET, POST, OPTIONS');
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.sendStatus(200);
 });
