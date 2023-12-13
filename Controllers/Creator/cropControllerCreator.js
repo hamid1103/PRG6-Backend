75f33e99cc43eba3ebcdc7960d01210b5d7a9150
hamid1103 <35217917+hamid1103@users.noreply.github.com>
2023-12-13 22:13:41 +0100

    Update cropControllerCreator.js

--- Controllers/Creator/cropControllerCreator.js
+++ Controllers/Creator/cropControllerCreator.js
@@ -24,7 +24,7 @@ const deleteCrop = async (req, res, next) => {
     const name = req.params.name
     try {
         await Crop.deleteOne({name: name}).exec();
-        res.status(200).json({message: "successfully removed " + name})
+        res.status(204).json({message: "successfully removed " + name})
     } catch (e) {
         next(e)
     }
